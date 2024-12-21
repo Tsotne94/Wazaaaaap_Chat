43b0220a16a7b33b82590be10386984e8adc7586
KochieviAna <anna.kochievi@gmail.com>
2024-12-21 19:12:55 +0100

    Reusable LabelAndTextFieldView and LoginPage added.

---
+++ Wazaaaaaaaap/Wazaaaaaaaap/Models/LoginView.swift
@@ -0,0 +1,113 @@
+//
+//  LoginView.swift
+//  Wazaaaaaaaap
+//
+//  Created by MacBook on 21.12.24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        ZStack{
+            backgroundColor
+                .ignoresSafeArea()
+            
+            VStack {
+                wazapLabel
+                
+                LabelAndTextFieldView(
+                    text: .constant(""),
+                    label: "Email",
+                    placeholder: "Your email address"
+                )
+                .padding(.top)
+                
+                LabelAndTextFieldView(
+                    text: .constant(""),
+                    label: "Password",
+                    placeholder: "Your password",
+                    isPassword: true
+                )
+                
+                HStack {
+                    newUserQuestionLabel
+                    
+                    Spacer()
+                    
+                    signUpButton
+                }
+                .padding(.leading, 32)
+                .padding(.trailing, 18)
+                
+                continueWithGoogleButton
+                    .padding(.horizontal)
+                    .padding(.top, 100)
+                
+                loginButton
+                    .padding()
+            }
+            .padding()
+        }
+    }
+    
+    private var backgroundColor: some View {
+        Color.customWhite
+    }
+    
+    private var wazapLabel: some View {
+        Text("Wazaaaaap")
+            .foregroundStyle(.primaryPurple)
+            .font(.custom("Pacifico-Regular", size: 48))
+    }
+    
+    private var newUserQuestionLabel: some View {
+        Text("New To Wazaaaaap?")
+            .foregroundStyle(.secondaryText)
+            .font(.custom("Inter", size: 12))
+    }
+    
+    private var signUpButton: some View {
+        Button("Sign Up") {}
+            .foregroundColor(.secondaryText)
+            .font(.custom("AnekDevanagari-Bold", size: 18))
+    }
+    
+    private var continueWithGoogleButton: some View {
+        Button(action: {
+            
+        }) {
+            HStack {
+                Image("google")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 23, height: 23)
+                
+                Text("Continue With Google")
+                    .font(.custom("Roboto-Medium", size: 20))
+                    .foregroundColor(.primaryBlack).opacity(0.54)
+            }
+            .padding(.horizontal, 49)
+            .padding(.top, 15)
+            .padding(.bottom, 15)
+            .background(.primaryWhite)
+            .cornerRadius(10)
+        }
+    }
+    
+    private var loginButton: some View {
+        Button("Log In") {}
+            .font(.custom("Inter_28pt-SemiBold", size: 20))
+            .padding(.horizontal, 134)
+            .padding(.top, 20)
+            .padding(.bottom, 20)
+            .foregroundColor(.primaryWhite)
+            .background(.primaryPurple)
+            .cornerRadius(12)
+    }
+}
+
+
+#Preview {
+    LoginView()
+}
---
+++ Wazaaaaaaaap/Wazaaaaaaaap/Resources/ReusableComponents/LabelAndTextFieldView.swift
@@ -0,0 +1,100 @@
+//
+//  LabelAndTextFieldView.swift
+//  Wazaaaaaaaap
+//
+//  Created by MacBook on 21.12.24.
+//
+
+import SwiftUI
+
+struct LabelAndTextFieldView: View {
+    @State private var isSecure: Bool = true
+    @Binding var text: String
+    var label: String
+    var placeholder: String
+    var isPassword: Bool = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            titleLabel
+                .padding(.leading, 4)
+            
+            HStack {
+                if isPassword {
+                    customLockImage
+                }
+                
+                if isPassword && isSecure {
+                    secureTextField
+                } else {
+                    regularTextField
+                }
+                
+                if isPassword {
+                    visibilityButton
+                }
+            }
+            .padding(.leading, 16)
+            .padding(.trailing, 16)
+            .padding(.bottom, 17)
+            .padding(.top, 17)
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(Color.gray.opacity(0.6), lineWidth: 1)
+            )
+        }
+        .padding(.vertical, 17)
+        .padding(.leading, 32)
+        .padding(.trailing, 15)
+    }
+    
+    private var titleLabel: some View {
+        Text(label)
+            .font(.headline)
+            .foregroundColor(.secondaryText)
+    }
+    
+    private var customLockImage: some View {
+        Image("customLock")
+            .foregroundColor(.secondaryText)
+    }
+    
+    private var secureTextField: some View {
+        SecureField(placeholder, text: $text)
+            .textFieldStyle(PlainTextFieldStyle())
+            .autocapitalization(.none)
+    }
+    
+    private var regularTextField: some View {
+        TextField(placeholder, text: $text)
+            .textFieldStyle(PlainTextFieldStyle())
+            .autocapitalization(.none)
+    }
+    
+    private var visibilityButton: some View {
+        Button(action: {
+            isSecure.toggle()
+        }) {
+            Image(systemName: isSecure ? "eye.slash" : "eye")
+                .foregroundColor(.primaryBlack)
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        LabelAndTextFieldView(
+            text: .constant(""),
+            label: "Email",
+            placeholder: "Enter your email"
+        )
+        
+        LabelAndTextFieldView(
+            text: .constant(""),
+            label: "Password",
+            placeholder: "Enter your password",
+            isPassword: true
+        )
+    }
+    .padding()
+}
