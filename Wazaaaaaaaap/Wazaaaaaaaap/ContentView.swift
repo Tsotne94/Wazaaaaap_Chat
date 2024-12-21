3322a6aa676692e510faedcf7e7feee76fe98d0f
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2024-12-22 02:13:34 +0400

    added test contenview

--- Wazaaaaaaaap/Wazaaaaaaaap/ContentView.swift
+++ Wazaaaaaaaap/Wazaaaaaaaap/ContentView.swift
@@ -7,16 +7,40 @@
 
 import SwiftUI
 import Firebase
+import FirebaseAuth
 
 struct ContentView: View {
+    @State var username: String = ""
+    @State var password: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "person.fill")
+                .resizable()
+                .frame(width: 120, height: 120)
+                .scaledToFill()
+            TextField("", text: $username, prompt: Text("Username"))
+                .padding()
+                .foregroundStyle(.green)
+                .background(.white)
+            TextField("", text: $password, prompt: Text("Username"))
+                .padding()
+                .foregroundStyle(.green)
+                .background(.white)
+            Button("press") {
+                signUp()
+            }
         }
         .padding()
+    }
+    func signUp() {
+        Auth.auth().createUser(withEmail: username, password: password) { result, err in
+            if let err = err {
+                print("failed \(err)")
+            } else {
+                print("yes success \(result)")
+            }
+            
+        }
     }
 }
 
