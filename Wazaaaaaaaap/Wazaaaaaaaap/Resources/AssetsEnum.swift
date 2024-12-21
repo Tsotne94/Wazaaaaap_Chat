66ae58ef62e7a258c498cce5946dfa0a399846ab
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2024-12-21 15:39:53 +0400

    template project with assets

---
+++ Wazaaaaaaaap/Wazaaaaaaaap/Resources/AssetsEnum.swift
@@ -0,0 +1,26 @@
+//
+//  AssetsEnum.swift
+//  Wazaaaaaaaap
+//
+//  Created by Cotne Chubinidze on 21.12.24.
+//
+import Foundation
+
+enum AppColors {
+    case red
+    case white
+    case purple
+    case textPrimary
+    case textSecondary
+}
+
+enum AppIcons {
+    case back
+    case eye
+    case gear
+    case lock
+    case send
+    case google
+    case noProfilePicture
+}
+
